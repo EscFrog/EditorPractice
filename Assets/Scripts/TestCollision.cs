5e99aa1d304f241928a1b0a2018c92c45c87f00c
Walter Park <neodust223@gmail.com>
2024-11-02 21:59:58 +0900

    카메라에서 레이를 쏴서 감지하는 법

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -21,14 +21,23 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        // Debug.Log(Input.mousePosition);   // Screen coordinate
+        // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));   // Viewport coordinate
 
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
-        foreach (RaycastHit hit in hits)
+
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log($"Raycast {hit.collider.gameObject.name}!");
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            Vector3 dir = mousePos - Camera.main.transform.position;
+            dir = dir.normalized;
+
+            Debug.DrawRay(Camera.main.transform.position, dir * 1000.0f, Color.red, 1.0f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 1000.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
         }
     }
 }
