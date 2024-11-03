4e258da9b3442b044d5e27b9ed91a8584f808801
Walter Park <neodust223@gmail.com>
2024-11-03 14:21:08 +0900

    회전을 부드럽게

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -36,7 +36,8 @@ public class PlayerController : MonoBehaviour
             {
                 float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
                 transform.position += dir.normalized * moveDist;
-                transform.LookAt(_destPos);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), _rotateSpeed);
             }
         }
     }
