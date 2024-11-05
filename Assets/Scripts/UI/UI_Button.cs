3cd75c24191848e94657633c7b961a1e58bca24a
Walter Park <neodust223@gmail.com>
2024-11-05 19:10:41 +0900

    자동으로 ui컴포넌트를 찾는 코드 구현

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Button : MonoBehaviour
 {
-    [SerializeField]
-    Text _text;
+    Dictionary<Type, UnityEngine.Object[]> _objectsDict =
+        new Dictionary<Type, UnityEngine.Object[]>(); // 타입별로 오브젝트 리스트가 매치되는 딕셔너리
+
+    enum Buttons
+    {
+        PointButton
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText,
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+    }
+
+    void Bind<T>(Type type)
+        where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objectsDict.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] = Utils.FindChild<T>(gameObject, names[i], true);
+        }
+    }
 
     int _score = 0;
 
     public void OnButtonClicked()
     {
         _score++;
-        _text.text = $"점수 : {_score}";
     }
 }
