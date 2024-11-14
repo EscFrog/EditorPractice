34179f6e00171c2057c1e002212a51433c170ef2
Walter Park <neodust223@gmail.com>
2024-11-14 19:02:17 +0900

    코루틴 실습

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    Coroutine co;
+
     protected override void Init()
     {
         base.Init();
@@ -11,6 +13,29 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
+
+        co = StartCoroutine("CoExplodeAfterSeconds", 4.0f);
+        StartCoroutine("CoStopExplode", 2.0f);
+    }
+
+    IEnumerator CoStopExplode(float second)
+    {
+        Debug.Log("Stop Enter");
+        yield return new WaitForSeconds(second);
+        Debug.Log("Stop Execute!!!");
+        if (co != null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+    }
+
+    IEnumerator CoExplodeAfterSeconds(float second)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(second);
+        Debug.Log("Explode Excute!!");
+        co = null;
     }
 
     public override void Clear()
