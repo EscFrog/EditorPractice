de0e09b14c3f8a7f003f5da7b9e0353add6745cd
Walter Park <neodust223@gmail.com>
2024-11-15 10:46:32 +0900

    코루틴 관련 테스트 코드 삭제

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
-
     protected override void Init()
     {
         base.Init();
@@ -13,29 +11,6 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
-
-        co = StartCoroutine("CoExplodeAfterSeconds", 4.0f);
-        StartCoroutine("CoStopExplode", 2.0f);
-    }
-
-    IEnumerator CoStopExplode(float second)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(second);
-        Debug.Log("Stop Execute!!!");
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-
-    IEnumerator CoExplodeAfterSeconds(float second)
-    {
-        Debug.Log("Explode Enter");
-        yield return new WaitForSeconds(second);
-        Debug.Log("Explode Excute!!");
-        co = null;
     }
 
     public override void Clear()
