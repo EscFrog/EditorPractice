39dab6d0644f9951d0c31051feca511ca9b5479a
Walter Park <neodust223@gmail.com>
2024-11-18 12:24:23 +0900

    네임스페이스를 나눠서 코드 정리

--- Assets/Scripts/Data/Data_Contents.cs
+++ Assets/Scripts/Data/Data_Contents.cs
@@ -3,33 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-#region Stat
-
-[Serializable]
-public class Stat
+namespace Data
 {
-    public int level;
-    public int HP;
-    public int attack;
-}
+    #region Stat
 
-public interface ILoader<key, Value>
-{
-    Dictionary<key, Value> MakeDict();
-}
-
-[Serializable]
-public class StatData : ILoader<int, Stat>
-{
-    public List<Stat> stats = new List<Stat>();
-
-    public Dictionary<int, Stat> MakeDict()
+    public interface ILoader<key, Value>
     {
-        Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
-        foreach (Stat stat in stats)
-            dict.Add(stat.level, stat);
-        return dict;
+        Dictionary<key, Value> MakeDict();
     }
-}
 
-#endregion
+    [Serializable]
+    public class Stat
+    {
+        public int level;
+        public int HP;
+        public int attack;
+    }
+
+    [Serializable]
+    public class StatData : ILoader<int, Stat>
+    {
+        public List<Stat> stats = new List<Stat>();
+
+        public Dictionary<int, Stat> MakeDict()
+        {
+            Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
+            foreach (Stat stat in stats)
+                dict.Add(stat.level, stat);
+            return dict;
+        }
+    }
+
+    #endregion
+}
--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 
 public class DataManager
 {
-    public Dictionary<int, Stat> StatDict { get; private set; } = new Dictionary<int, Stat>();
+    public Dictionary<int, Data.Stat> StatDict { get; private set; } =
+        new Dictionary<int, Data.Stat>();
 
     public void Init()
     {
-        StatDict = LoadJson<StatData, int, Stat>("StatData").MakeDict();
+        StatDict = LoadJson<Data.StatData, int, Data.Stat>("StatData").MakeDict();
     }
 
     T LoadJson<T, key, Value>(string path)
-        where T : ILoader<key, Value>
+        where T : Data.ILoader<key, Value>
     {
         TextAsset textAsset = Managers.Resource.Load<TextAsset>($"Data/{path}");
         return JsonUtility.FromJson<T>(textAsset.text);
